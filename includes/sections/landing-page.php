f3f7a6af781b87b27a8f3da07b9945ea238ff4de
Samuel Cole <cole.samuel21@gmail.com>
2018-02-10 20:13:18 +0000

    added warning message for IE users

--- includes/sections/landing-page.php
+++ includes/sections/landing-page.php
@@ -3,6 +3,15 @@
         <!-- offset col -->
     </div>
     <div class="col-12 col-md-10">
+        <?php if(strpos($_SERVER["HTTP_USER_AGENT"], "MSIE")) { ?>
+            
+            <!-- echo warning message -->
+            <div class="col-md-6 col-10 alert alert-danger">
+                <p><strong>Warning: </strong>It looks like you are using Internet Explorer. This webpage is best viewed in a more modern browser such as Firefox, Google Chrome, or Safari.</p> 
+            </div>
+            
+        <?php } ?>
+            
         <!--<h1 class="brand center-text">SAMUEL <span>COLE</span></h1>-->
         <h1 class="center-text"><img src="includes/img/cole-logo-simple.svg" alt="Samuel Cole" class="img-fluid"></img></h1>
         <h3 class="center-text">Web Developer</h3>
