468a57bd8fd10046d8ee5c25f26b172733cef106
Samuel Cole <cole.samuel21@gmail.com>
2018-02-12 22:07:10 +0000

    created portfolio page

---
+++ includes/sections/portfolio-page.php
@@ -0,0 +1,91 @@
+<div class="row justify-content-center">
+	<div class="col-10 col-hg-8">
+		<div class="row justify-content-center">
+			<div class="col-12">
+				<h3 class="ml-25">Portfolio</h3>
+				<hr>
+			</div>
+		</div>
+		<div class="row justify-content-center" id="portfolio-items">
+			<div class="col-12 col-md-10 col-lg-12">
+				<div class="row justify-content-center portfolio-item">
+					<div class="col-12 col-lg-5">
+						<img class="img-fluid" src="includes/img/php-blog.jpg">
+					</div>
+					<div class="col-12 col-lg-7">
+						<h5 class="text-center">PHP Laravel Blog CMS</h5>
+	                    <p>
+							This is a content management system for a blog website that runs on the Laravel PHP framework. 
+							This is a personal project that I started in order to learn more about Laravel and MVC frameworks. 
+							At the moment, the administrator can make, edit, and delete blog posts, categories, and tags from
+							the administrative portion of the website. Regular users can make an account or view the published
+							blog posts.
+						</p> 
+						<p>To create this I used:</p>
+						<ul>
+							<li>HTML5, CSS, JavaScript</li>
+							<li>PHP, Laravel 5.2</li>
+						</ul>
+						<div class="row justify-content-center">
+							<div class="col-12">
+								<a class="btn btn-custom form-control" href="https://github.com/colesam/Laravel-Blog" target="_blank">View Files on GitHub</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				<hr>
+				
+				<div class="row justify-content-center portfolio-item">
+					<div class="col-12 col-lg-5">
+						<img class="img-fluid" src="includes/img/chess-club-logo.jpg">
+					</div>
+					<div class="col-12 col-lg-7">
+						<h5 class="text-center">UST Chess Club Logo</h5>
+	                    <p>
+							As Marketing Chair for the University of St. Thomas Chess Club, one of my responsibilities 
+							was to create a logo to be used in university advertisements and on club t-shirts/gear. As 
+							the first Marketing Chair for the UST Chess Club, I had to create this logo from scratch using 
+							Adobe Illustrator. 
+						</p> 
+						<p>To create this I used:</p>
+						<ul>
+							<li>Adobe Illustrator CC</li>
+						</ul>
+						<div class="row justify-content-center">
+							<div class="col-12">
+								<a class="btn btn-custom form-control" href="https://github.com/colesam/Chess-Club-Logo" target="_blank">View Files on GitHub</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				<hr>
+				
+				<div class="row justify-content-center portfolio-item">
+					<div class="col-12 col-lg-5">
+						<img class="img-fluid" src="includes/img/wireframes.jpg">
+					</div>
+					<div class="col-12 col-lg-7">
+						<h5 class="text-center">UST Newsroom Wireframes</h5>
+	                    <p>
+							While working as an intern for the University of St. Thomas Web Services, I helped with a project to 
+							transition our <a class="link" href="https://stthomas.edu/news" target="_blank">Newsroom</a> to a new 
+							Wordpress theme. My primary responsibility was to create these wireframes in Adobe Illustrator. 
+							Unfortunately the project has been placed on hold, so the changes have not been made to the live site. 
+						</p> 
+						<p>To create this I used:</p>
+						<ul>
+							<li>Adobe Illustrator CC</li>
+						</ul>
+						<div class="row justify-content-center">
+							<div class="col-12">
+								<a class="btn btn-custom form-control" href="https://github.com/colesam/Chess-Club-Logo" target="_blank">View Files on GitHub</a>
+							</div>
+						</div>
+					</div>
+				</div>				
+			</div>
+		</div>
+	</div>
+</div>
