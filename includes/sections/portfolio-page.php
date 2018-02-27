964cbd4bbfd7eb3fa35f2c9a5047cb388e8edbbb
Samuel Cole <cole.samuel21@gmail.com>
2018-02-27 20:27:05 +0000

    added new item to the portfolio section

--- includes/sections/portfolio-page.php
+++ includes/sections/portfolio-page.php
@@ -17,7 +17,7 @@
 	                    <p>
 							This is a content management system for a blog website that runs on the Laravel PHP framework. 
 							This is a personal project that I started in order to learn more about Laravel and MVC frameworks. 
-							At the moment, the administrator can make, edit, and delete blog posts, categories, and tags from
+							At the moment, the administrator can create, edit, and delete blog posts, categories, and tags using
 							the administrative portion of the website. Regular users can make an account or view the published
 							blog posts.
 						</p> 
@@ -29,6 +29,61 @@
 						<div class="row justify-content-center">
 							<div class="col-12">
 								<a class="btn btn-custom form-control" href="https://github.com/colesam/Laravel-Blog" target="_blank">View Files on GitHub</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				<hr>
+				
+				<div class="row justify-content-center portfolio-item">
+					<div class="col-12 col-lg-5">
+						<img class="img-fluid" src="includes/img/web-accessibility.jpg">
+					</div>
+					<div class="col-12 col-lg-7">
+						<h5 class="text-center">Web Accessibility Website</h5>
+	                    <p>
+							My current task at UST Web Services is to create a website that explains web accessibility to different members of our University. 
+							Within this project, the biggest priority for me right now is to create a stunning landing page that draws visitors in. The image 
+							to the left shows three banners I created according to University branding guidelines that will accompany the three reasons web 
+							accessibility is important: moral duty, quality assurance, and legal responsibility.
+						</p> 
+						<p>To create this I am using:</p>
+						<ul>
+							<li>HTML5</li>
+							<li>CSS</li>
+							<li>Foundations CSS Framework</li>
+							<li>Adobe Illustrator</li>
+						</ul>
+						<div class="row justify-content-center">
+							<div class="col-12">
+								<a class="btn btn-custom form-control" href="https://github.com/colesam/Number-Guessing-Game" target="_blank">View Files on GitHub</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				<hr>
+				
+				<div class="row justify-content-center portfolio-item">
+					<div class="col-12 col-lg-5">
+						<img class="img-fluid" src="includes/img/chess-club-logo.jpg">
+					</div>
+					<div class="col-12 col-lg-7">
+						<h5 class="text-center">Chess Club Logo</h5>
+	                    <p>
+							As Marketing Chair for the University of St. Thomas Chess Club, one of my responsibilities 
+							was to create a logo to be used in university advertisements and on club t-shirts/gear. As 
+							the first Marketing Chair for the UST Chess Club, I had to create this logo from scratch using 
+							Adobe Illustrator. 
+						</p> 
+						<p>To create this I used:</p>
+						<ul>
+							<li>Adobe Illustrator CC</li>
+						</ul>
+						<div class="row justify-content-center">
+							<div class="col-12">
+								<a class="btn btn-custom form-control" href="https://github.com/colesam/Chess-Club-Logo" target="_blank">View Files on GitHub</a>
 							</div>
 						</div>
 					</div>
@@ -56,32 +111,6 @@
 						<div class="row justify-content-center">
 							<div class="col-12">
 								<a class="btn btn-custom form-control" href="https://github.com/colesam/Number-Guessing-Game" target="_blank">View Files on GitHub</a>
-							</div>
-						</div>
-					</div>
-				</div>
-				
-				<hr>
-				
-				<div class="row justify-content-center portfolio-item">
-					<div class="col-12 col-lg-5">
-						<img class="img-fluid" src="includes/img/chess-club-logo.jpg">
-					</div>
-					<div class="col-12 col-lg-7">
-						<h5 class="text-center">UST Chess Club Logo</h5>
-	                    <p>
-							As Marketing Chair for the University of St. Thomas Chess Club, one of my responsibilities 
-							was to create a logo to be used in university advertisements and on club t-shirts/gear. As 
-							the first Marketing Chair for the UST Chess Club, I had to create this logo from scratch using 
-							Adobe Illustrator. 
-						</p> 
-						<p>To create this I used:</p>
-						<ul>
-							<li>Adobe Illustrator CC</li>
-						</ul>
-						<div class="row justify-content-center">
-							<div class="col-12">
-								<a class="btn btn-custom form-control" href="https://github.com/colesam/Chess-Club-Logo" target="_blank">View Files on GitHub</a>
 							</div>
 						</div>
 					</div>
