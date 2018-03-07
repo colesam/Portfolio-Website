4d3131a38a18d06aa6e1d3a2897c489edfbec373
Samuel Cole <cole.samuel21@gmail.com>
2018-03-07 15:52:27 +0000

    updated html and css on landing page

--- includes/sections/landing-page.php
+++ includes/sections/landing-page.php
@@ -1,12 +1,19 @@
 <div class="container-fluid">
-	<div class="row d-flex flex-row justify-content-between align-items-stretch">
-		<div class="landing-header d-flex flex-column justify-content-center">
-			<h1>SAMUEL COLE</h1>
-			<h2>Web Development and Design</h2>
-			<button class="btn btn-custom about-nav">Get Started</button>
+	<div class="row d-flex flex-column flex-md-row justify-content-start justify-content-md-between align-items-stretch">
+		<div class="landing-graphic landing-graphic-sm d-flex flex-column justify-content-center align-items-center d-md-none">
+			<img src="includes/img/landing-display-sm.jpg" class="img-fluid"></img>
 		</div>
-		<div class="landing-graphic">
-			test
+		<div class="landing-header d-flex flex-column align-items-center align-items-md-start justify-content-md-center">
+			<h1>SAMUEL COLE</h1>
+			<div>
+				<h2>Web Development and Design</h2>
+				<button class="btn form-control btn-custom about-nav">Get Started</button>
+			</div>
+		</div>
+		<div class="landing-graphic landing-graphic-lg d-none d-md-flex flex-column justify-content-center align-items-center">
+			<img	src="includes/img/landing-display-sm.jpg" 
+					data-src-1200px="includes/img/landing-display-lg.jpg"
+					class="img-fluid"></img>
 		</div>
 	</div>
 </div>
